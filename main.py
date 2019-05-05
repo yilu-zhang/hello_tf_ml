8a8622d82fc103d4c4ecb58a801db61c7cde65b3
yiluzhang <861280152@qq.com>
2019-05-05 20:04:57 +0800

    create tensor_session_test()

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 # author:yiluzhang
 
-import tensorflow as tf
+#import tensorflow as tf
+import starting
 
 if __name__ == '__main__':
     print("hello tensorflow")
+    starting.tensor_session_test()
