9b4610ce7ed80e28795ada395c4b304f116dba50
yiluzhang <861280152@qq.com>
2019-05-19 21:57:31 +0800

    完成《tensorflow》所有学习，7-10章未上手代码，只是大概看了下

--- main.py
+++ main.py
@@ -5,8 +5,13 @@
 #import tensorflow as tf
 import starting
 import mnist_train
+import LeNet5_train
 
 if __name__ == '__main__':
     # print("hello tensorflow")
     # starting.tensor_session_test()
-    mnist_train.train_action()
+    # mnist_train.mnist_normal_train()
+    # LeNet5_train.mnist_normal_train()  # 半个多小时跑完1000个迭代（loss:4->0.7)
+    # starting.namespace_test()
+    starting.device_test()
+
